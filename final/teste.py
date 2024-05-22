69ccd494a0faba93ccdffd599b24cfe2e15d2f5b
lucasss45 <nota10.lucas@gmail.com>
2024-05-22 17:40:22 -0300

    ADD teste.py
    criando o teste.py que sera basicamente o executavel com o codigo da IA que vai operar as funções e fazer a analise

---
+++ final/teste.py
@@ -0,0 +1,10 @@
+import time
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+import extrator_POSICAO as posicao
+import extrator_ALTURA as altura
+import extrator_PROXIMIDADE as proximidade
+import alfabeto
+
