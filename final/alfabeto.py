117ca4523455aeeb11ba8005063392b697f11560
lucasss45 <nota10.lucas@gmail.com>
2024-05-22 17:37:15 -0300

    Update alfabeto.py
    adicionando as letras N, O, P, Q, R, S, T, U, V, W

--- final/alfabeto.py
+++ final/alfabeto.py
@@ -8,6 +8,16 @@ G = ['polegar esticado vertical: proximo ao indicador', 'indicador esticado vert
 I = ['polegar dobrado: proximo ao indicador', 'indicador dobrado: proximo ao medio', 'medio dobrado: proximo ao anelar', 'anelar dobrado: afastado do minimo', 'minimo esticado vertical: afastado do anelar']
 L = ['polegar esticado vertical: afastado do indicador', 'indicador esticado vertical: afastado do medio', 'medio dobrado: proximo ao anelar', 'anelar dobrado: proximo ao minimo', 'minimo dobrado: proximo ao anelar']
 M = ['polegar esticado vertical: proximo ao indicador', 'indicador esticado vertical: proximo ao medio', 'medio esticado vertical: proximo ao anelar', 'anelar esticado vertical: proximo ao minimo', 'minimo esticado vertical: proximo ao anelar']
+N = ['polegar esticado horizontal: afastado do indicador', 'indicador esticado vertical: proximo ao medio', 'medio esticado vertical: afastado do anelar', 'anelar dobrado: proximo ao minimo', 'minimo dobrado: proximo ao anelar']
+O = ['polegar esticado horizontal: afastado do indicador', 'indicador dobrado: proximo ao medio', 'medio dobrado: proximo ao anelar', 'anelar dobrado: proximo ao minimo', 'minimo dobrado: proximo ao anelar']
+P = ['polegar esticado vertical: afastado do indicador', 'indicador esticado vertical: afastado do medio', 'medio esticado vertical: afastado do anelar', 'anelar dobrado: proximo ao minimo', 'minimo dobrado: proximo ao anelar']
+Q = ['polegar esticado vertical: afastado do indicador', 'indicador esticado vertical: afastado do medio', 'medio dobrado: proximo ao anelar', 'anelar dobrado: afastado do minimo', 'minimo esticado vertical: afastado do anelar']
+R = ['polegar dobrado: afastado do indicador', 'indicador esticado vertical: proximo ao medio', 'medio esticado vertical: afastado do anelar', 'anelar dobrado: proximo ao minimo', 'minimo dobrado: proximo ao anelar']
+S = ['polegar dobrado: proximo ao indicador', 'indicador dobrado: proximo ao medio', 'medio dobrado: proximo ao anelar', 'anelar dobrado: proximo ao minimo', 'minimo dobrado: proximo ao anelar']
+T = ['polegar esticado horizontal: afastado do indicador', 'indicador dobrado: afastado do medio', 'medio esticado vertical: proximo ao anelar', 'anelar esticado vertical: proximo ao minimo', 'minimo esticado vertical: proximo ao anelar']
+U = ['polegar esticado horizontal: afastado do indicador', 'indicador esticado vertical: proximo ao medio', 'medio esticado vertical: afastado do anelar', 'anelar dobrado: proximo ao minimo', 'minimo dobrado: proximo ao anelar']
+V = ['polegar esticado horizontal: afastado do indicador', 'indicador esticado vertical: afastado do medio', 'medio esticado vertical: afastado do anelar', 'anelar dobrado: proximo ao minimo', 'minimo dobrado: proximo ao anelar']
+W = ['polegar esticado horizontal: afastado do indicador', 'indicador esticado vertical: afastado do medio', 'medio esticado vertical: proximo ao anelar', 'anelar esticado vertical: afastado do minimo', 'minimo dobrado: afastado do anelar']
 
 letras = [A, B, C, D, E, F, G, I, L, M, N, O, P, Q, R, S, T, U, V, W]
 
