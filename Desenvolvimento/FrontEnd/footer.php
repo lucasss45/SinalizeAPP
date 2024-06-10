50f6d8c62fa1a75ed79ad36d6f409db412e467fc
Kali-Gomes <108465289+Kali-Gomes@users.noreply.github.com>
2024-06-10 15:02:09 -0300

    Design Remade

--- Desenvolvimento/FrontEnd/footer.php
+++ Desenvolvimento/FrontEnd/footer.php
@@ -1,13 +1,2 @@
-<footer class="d-flex justify-content-between position-fixed bottom-0">
-        <a href="Aulas.php">
-            <img class="placeholder" src="Imagens/AulasImg.png" alt="Aulas">
-        </a>
-        <a href="Tradutor.php">
-            <img class="placeholder" src="Imagens/TradutorImg.png" alt="Tradutor">
-        </a>
-        <a href="Dicionario.php">
-            <img class="placeholder" src="Imagens/DicionarioImg.png" alt="Dicionário">
-        </a>
-    </footer>
 </body>
 </html>
--- Desenvolvimento/FrontEnd/header.php
+++ Desenvolvimento/FrontEnd/header.php
@@ -11,10 +11,12 @@
      <!-- SCRIPT LOCAL -->
      <script src="script.js"></script>
    
-    <!-- FONTE -->
+    <!-- FONTES -->
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Dela+Gothic+One&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
     </style>
+
     <!-- BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
@@ -24,20 +26,32 @@
 
 </head>
 <body>
-
-    <div>       
-        <header class="d-flex justify-content-between p-1">
-            <div class="dropdown">
-                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <img src="Imagens/HamburguerMenu.png" alt="Hamburguer Menu" width="30" height="30">
-                </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a class="dropdown-item" href="CadastroUsuario/dashboard.php">Perfil</a>
-                    <a class="dropdown-item" href="Configuracoes.php">Configurações</a>
-                    <a class="dropdown-item" href="Comunidade.php">Comunidade</a>
-                </div>
-              </div> 
-            <h1 class="align-self-center text-warp m-1 fs-1">SINALIZE</h1> 
-            <img class="logo img-fluid" src="Imagens/Logo_SinalizeV2.png" alt="LogoSinalize">
-        </header>
-    </div>
+    <header class="row p-3">
+        <div class="col">
+            <img class="HamburguerMenu" src="Imagens/Logo_SinalizeV2.png" alt="Logo da Sinalize">
+        </div>
+        <div class="col col-lg-6">
+            <input class="SearchBar" type="text" placeholder="Pesquisar aulas">
+        </div>
+        <div class="col">
+            <img class="ProfileButton" src="Imagens/ExPictureHere.png" alt="Botão de perfil do usuário">
+        </div>
+    </header>
+    <aside class="p-3">
+        <div class="row mb-3">
+            <a>
+                <img src="Imagens/AulasImg.png" alt="Botão da página das aulas">           
+            </a>
+        </div>
+        <div class="row mb-3">
+            <a>
+                <img src="Imagens/DicionarioImg.png" alt="Botão da página de dicionário">
+            </a>
+        </div>
+        <div class="row">
+            <a>
+                <img class="TradImg" src="Imagens/Tradutor.png" alt="Botão da função de tradutor">
+            </a>
+        </div>
+        
+</aside>
