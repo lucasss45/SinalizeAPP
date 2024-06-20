b880829e37d623a211201979f3216aa360a5e286
Lucas Vizeu <nota10.lucas@gmail.com>
2024-06-20 12:19:34 -0300

    Update geral

--- Desenvolvimento/FrontEnd/Tradutor.php
+++ Desenvolvimento/FrontEnd/Tradutor.php
@@ -2,7 +2,20 @@
     include 'header.php';
 ?>
 
+<?php
+    
+
+    if(isset($_POST['executar'])) {
+        exec('python seu_script.py');
+        echo "Script executado com sucesso!";
+    }
+?>
     <main class="d-flex justify-content-center">
+        <div class="col-lg-3 d-flex justify-content-end align-items-center">
+            <button id="btn-executar" class="btn-header">
+                <p>Dale</p>
+            </button>
+        </div>
     </main>
     
 <?php
